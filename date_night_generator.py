88bb4ecea905b962b92bbc05d0bddbe7a52ecfec
Nick <nsteed82189@yahoo.com>
2018-07-07 04:31:03 -0400

    while menu_select infinite loop fix
    Program was stuck in infinite loop as the parameter: while menu_select == "" could never be satisfied by user

--- date_night_generator.py
+++ date_night_generator.py
@@ -1,20 +1,15 @@
 import random
 
-date_ideas = ["option 1", "option 2", "option 3", "option 4", "option 5"]
+date_ideas = ["Movies", "Walk in the Park", "Picnic", "Bowling", "Painting"]
 
 def start():
-	'''Prompts user with an opening menu asking for selection'''
-	menu_select = ""
-	while menu_select == "":
-		print ("1 Get Date Idea, 2 Add a Date, 0 to Exit")
-		'''0 may not exist in mobile app'''
-		menu_select = input("Please make a selection")
-		if menu_select == "1":
-			print(random.choice(date_ideas))
-		'''Allows user to add their own date idea to the original list'''
-		elif menu_select == "2": 
-			 user_date = input("What would you like to add?")
-			 date_ideas.append(user_date)
-
-
-
+        '''Prompts user with an opening menu asking for selection'''
+        menu_select = ""
+        while menu_select == "":
+                menu_select = input("1 Get Date Idea, 2 Add a Date, 0 to Exit")
+                if menu_select == "1":
+                        print(random.choice(date_ideas))
+                elif menu_select == "2": 
+                        user_date = input("What would you like to add?")
+                        date_ideas.append(user_date)
+                
